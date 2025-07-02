0f39d0144243c5093706eb62dc3f94743d7bbe4d
Naufal77-code <bglmaing@gmail.com>
2025-07-02 09:40:57 +0700

    asidjuf

--- src/purify/RiwayatBlokir.java
+++ src/purify/RiwayatBlokir.java
@@ -5,7 +5,7 @@ import javafx.beans.property.SimpleStringProperty;
 public class RiwayatBlokir {
     private final SimpleStringProperty nomor;
     private final SimpleStringProperty riwayat;
-
+    
     public RiwayatBlokir(String nomor, String riwayat) {
         this.nomor = new SimpleStringProperty(nomor);
         this.riwayat = new SimpleStringProperty(riwayat);
